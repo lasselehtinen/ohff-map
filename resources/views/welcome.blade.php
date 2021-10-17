ff4ad1d13517769c8367f601e149e708d241d7fb
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2021-10-17 21:50:49 +0300

    Small layout adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
   </head>
   <body>
     <form action="/" method="get">
-        &nbsp;&nbsp;&nbsp;
         <label for="activated_by">Activated by:</label>
         <input type="text" id="activated_by" name="filter[activated_by]" size="10" placeholder="Callsign" value="{{strtoupper($request->input('filter.activated_by'))}}">
         
@@ -29,8 +28,6 @@
       Activated 3 years ago: <img src="http://maps.google.com/intl/en_us/mapfiles/ms/micons/orange.png" width="16" height="16">
       Activated 4++ years ago: <img src="http://maps.google.com/intl/en_us/mapfiles/ms/micons/red.png" width="16" height="16">
     </div>
-
-    <br/>
 
     <div id="map"></div>
 
