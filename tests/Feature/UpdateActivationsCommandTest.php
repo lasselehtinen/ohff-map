b09d13df62b7e12917f914c62b63983a6155a878
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2021-10-27 21:04:09 +0300

    Added date checks

--- tests/Feature/UpdateActivationsCommandTest.php
+++ tests/Feature/UpdateActivationsCommandTest.php
@@ -35,6 +35,10 @@ class UpdateActivationsCommandTest extends TestCase
         // OH2BAV should have three activations
         $user = User::where('callsign', 'OH2BAV')->with('activations')->first();
         $this->assertSame(3, $user->activations->where('reference', 'OHFF-1079')->count());
+        $firstActivation = $user->activations->where('reference', 'OHFF-1079')->sortBy('pivot.activation_date')->first();
+        $latestActivation = $user->activations->where('reference', 'OHFF-1079')->sortByDesc('pivot.activation_date')->first();
+        $this->assertSame('2021-03-07', date('Y-m-d', strtotime($firstActivation->pivot->activation_date)));
+        $this->assertSame('2021-06-13', date('Y-m-d', strtotime($latestActivation->pivot->activation_date)));
 
         // OH2NOS should have one activation
         $user = User::where('callsign', 'OH2NOS')->with('activations')->first();
