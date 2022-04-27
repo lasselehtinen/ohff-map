ced0c89cd509d6c07de60ca10bb1bf8c6b9493c9
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2022-04-27 20:31:54 +0300

    Fixed Github issue link

--- public/js/index.js
+++ public/js/index.js
@@ -74,16 +74,17 @@ function initMap() {
     '<h2>'+ event.feature.getProperty("name") +'</h2>' +
     '<div id="bodyContent">' +
     '<p>Latest activation: ' + event.feature.getProperty("latest_activation_date") + ' by ' + event.feature.getProperty("latest_activator") + '</p>' +
-    '<p><a href='+ event.feature.getProperty("karttapaikka_link") + '" target="_new">Kansalaisen karttapaikka</a>';
+    '<p><a href='+ event.feature.getProperty("karttapaikka_link") + '" target="_new">Kansalaisen karttapaikka</a><br/>';
 
     if (event.feature.getProperty("wdpa_id")) {
-      contentString += '<br/><a href=https://www.protectedplanet.net/'+ event.feature.getProperty("wdpa_id") + '" target="_new">Protected Planet</a>';
+      contentString += '<a href=https://www.protectedplanet.net/'+ event.feature.getProperty("wdpa_id") + '" target="_new">Protected Planet</a><br/>';
     }
 
-    contentString += '</p>'
-
     // Add link to create github issue
-    contentString += '<p><a href="https://github.com/lasselehtinen/ohff-map/issues/new?template=reporting-issue-with-reference-information.md&title=&title=Problem%20with%20' + event.feature.getProperty("reference") + '">Report issue</a></p>'
+    contentString += '<a href="https://github.com/lasselehtinen/ohff-map/issues/new?template=reporting-issue-with-reference-information.md&title=&title=Problem%20with%20' + event.feature.getProperty("reference") + '">Report issue</a></br>'
+
+    contentString += '</p>'
+  
 
     infowindow.setContent(contentString); // show the html variable in the infowindow
     
