5a3031de2a62434f9eaf2158adc1db2fab8c626f
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2022-08-14 20:39:18 +0300

    Added command to check Protected Planet links

---
+++ app/Console/Commands/CheckProtectedPlanetLinks.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Reference;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class CheckProtectedPlanetLinks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:protected_planet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check the Protected Planet links';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $references = Reference::whereNotNull('wdpa_id')->get();
+
+        // Create progress bar
+        $bar = $this->output->createProgressBar($references->count());
+        $bar->setFormat('very_verbose');
+        $bar->start();
+
+        foreach ($references as $reference) {
+            $response = Http::get('https://www.protectedplanet.net/'.$reference->wdpa_id);
+
+            if ($response->status() === 500) {
+                $this->info($reference->reference);
+            }
+
+            sleep(3);
+            $bar->advance();
+        }
+
+        $bar->finish();
+
+        return 0;
+    }
+}
