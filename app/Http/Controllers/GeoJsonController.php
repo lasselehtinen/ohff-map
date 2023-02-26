4acfe99dad613809ce10f21badf911e658c9a175
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2023-02-26 19:19:22 +0200

    Static fixes

--- app/Http/Controllers/GeoJsonController.php
+++ app/Http/Controllers/GeoJsonController.php
@@ -55,25 +55,25 @@ class GeoJsonController extends Controller
         foreach ($references as $reference) {
             // Define properties
             $properties = [
-                'reference' => $reference->reference,
+                'reference' => $reference->reference, /** @phpstan-ignore-line */
                 'is_activated' => ! empty($reference->first_activation_date),
-                'first_activation_date' => $reference->first_activation_date,
-                'latest_activation_date' => $reference->latest_activation_date,
-                'latest_activator' => $reference->activators->sortByDesc('pivot.activation_date')->pluck('callsign')->first(),
-                'name' => $reference->name,
+                'first_activation_date' => $reference->first_activation_date, /** @phpstan-ignore-line */
+                'latest_activation_date' => $reference->latest_activation_date, /** @phpstan-ignore-line */
+                'latest_activator' => $reference->activators->sortByDesc('pivot.activation_date')->pluck('callsign')->first(), /** @phpstan-ignore-line */
+                'name' => $reference->name, /** @phpstan-ignore-line */
                 'icon' => $this->getIcon($reference),
-                'wdpa_id' => $reference->wdpa_id,
+                'wdpa_id' => $reference->wdpa_id, /** @phpstan-ignore-line */
                 'karttapaikka_link' => $this->getKansalaisenKarttaPaikkaLink($reference),
                 'paikkatietoikkuna_link' => $this->getPaikkatietoLink($reference),
-                'is_natura_2000_area' => (bool) $reference->natura_2000_area,
-                'notes' => $reference->notes,
+                'is_natura_2000_area' => (bool) $reference->natura_2000_area, /** @phpstan-ignore-line */
+                'notes' => $reference->notes, /** @phpstan-ignore-line */
             ];
 
-            $feature = new Feature($reference->location->jsonSerialize(), $properties);
+            $feature = new Feature($reference->location->jsonSerialize(), $properties); /** @phpstan-ignore-line */
             $features->push($feature);
 
             // Add geometry as a feature if zoom level is high enough
-            if ($request->input('zoom', 5) > 7 && ! is_null($reference->area)) {
+            if ($request->input('zoom', 5) > 7 && ! is_null($reference->area)) { /** @phpstan-ignore-line */
                 $feature = new Feature($reference->area->jsonSerialize(), $properties);
                 $features->push($feature);
             }
@@ -81,6 +81,7 @@ class GeoJsonController extends Controller
 
         $featureCollection = new FeatureCollection($features->toArray());
 
+        /** @phpstan-ignore-next-line */
         return response($featureCollection, 200, ['Content-Type => application/json']);
     }
 
@@ -92,17 +93,17 @@ class GeoJsonController extends Controller
      */
     public function getIcon($reference)
     {
-        if (in_array($reference->approval_status, ['received', 'approved'])) {
+        if (in_array($reference->approval_status, ['received', 'approved'])) { /** @phpstan-ignore-line */
             return 'https://maps.google.com/intl/en_us/mapfiles/ms/micons/purple.png';
         }
 
-        if (is_null($reference->first_activation_date)) {
+        if (is_null($reference->first_activation_date)) { /** @phpstan-ignore-line */
             return 'https://maps.google.com/intl/en_us/mapfiles/ms/micons/tree.png';
         }
 
         // Calculate years from latest activation
         $currentDate = new DateTime();
-        $latestActivation = new DateTime($reference->latest_activation_date);
+        $latestActivation = new DateTime($reference->latest_activation_date); /** @phpstan-ignore-line */
         $diff = $currentDate->diff($latestActivation);
 
         switch ($diff->y) {
@@ -140,8 +141,8 @@ class GeoJsonController extends Controller
         // Converting from WGS 84 to ETRS89
         $from = GeographicPoint::create(
             Geographic2D::fromSRID(Geographic2D::EPSG_WGS_84),
-            new Degree($reference->location->getLat()),
-            new Degree($reference->location->getLng()),
+            new Degree($reference->location->getLat()), /** @phpstan-ignore-line */
+            new Degree($reference->location->getLng()), /** @phpstan-ignore-line */
             null
         );
         $toCRS = Projected::fromSRID(Projected::EPSG_ETRS89_TM35FIN_N_E);
@@ -167,8 +168,8 @@ class GeoJsonController extends Controller
         // Converting from WGS 84 to ETRS89
         $from = GeographicPoint::create(
             Geographic2D::fromSRID(Geographic2D::EPSG_WGS_84),
-            new Degree($reference->location->getLat()),
-            new Degree($reference->location->getLng()),
+            new Degree($reference->location->getLat()), /** @phpstan-ignore-line */
+            new Degree($reference->location->getLng()), /** @phpstan-ignore-line */
             null
         );
 
