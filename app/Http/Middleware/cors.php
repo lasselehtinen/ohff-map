1e06ade8a479eeb8d34896a4a03059cf09eea0f1
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2023-04-16 13:13:44 +0300

    Style fixes

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -10,12 +10,11 @@ class cors
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin','*');
+        return $next($request)->header('Access-Control-Allow-Origin', '*');
     }
 }
