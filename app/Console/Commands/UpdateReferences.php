a9f052efa3c7b29ef329974d6dfb85909b98f04c
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2023-11-17 21:56:13 +0200

    Fixed approval status issue

--- app/Console/Commands/UpdateReferences.php
+++ app/Console/Commands/UpdateReferences.php
@@ -121,8 +121,8 @@ class UpdateReferences extends Command
             $continent = Continent::where('name', $sourceReference['continent'])->first();
             $reference->continent()->associate($continent); /** @phpstan-ignore-line */
 
-            // Check if new reference has been approved
-            if ($sourceReference['status'] === 'active' && $reference->approval_status === 'approved') { /** @phpstan-ignore-line */
+            // Check if new reference has been approved or already active
+            if ($sourceReference['status'] === 'active' || $reference->approval_status === 'approved') { /** @phpstan-ignore-line */
                 $reference->approval_status = 'saved';
             }
 
