9f1ad8afb673f1bb7abb7eb64a86ae8c3b33d7b7
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2023-11-19 21:14:57 +0200

    Adjusted cache warmup urls

--- app/Console/Commands/CacheWarmup.php
+++ app/Console/Commands/CacheWarmup.php
@@ -33,8 +33,19 @@ class CacheWarmup extends Command
         Artisan::call('responsecache:clear');
 
         // Warm up cache
-        Http::timeout(600)->get('https://kartta.ohff.fi/geojson');
-        Http::timeout(600)->get('https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bapproval_status%5D=');
+        $urls = [
+            'https://kartta.ohff.fi/geojson',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bapproval_status%5D=',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bnot_activated%5D=on&filter%5Bapproval_status%5D=',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bactivited_this_year%5D=on&filter%5Bapproval_status%5D=',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bapproval_status%5D=received',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bapproval_status%5D=approved',
+            'https://kartta.ohff.fi/geojson?filter%5Bactivated_by%5D=&filter%5Bnot_activated_by%5D=&filter%5Breference%5D=&filter%5Bapproval_status%5D=saved',
+        ];
+
+        foreach ($urls as $url) {
+            Http::timeout(600)->get($url);
+        }
 
         return 0;
     }
